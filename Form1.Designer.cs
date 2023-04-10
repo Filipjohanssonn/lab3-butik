71f8f39c0cbca52c3708c4061adb2c7b17bbeb83
Filip Johansson <62192077+Filipjohanssonn@users.noreply.github.com>
2023-04-10 15:41:34 +0200

    1'st commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,385 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Laboration_4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.dataGridView3 = new System.Windows.Forms.DataGridView();
+            this.dataGridViewTextBoxColumn3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.FormatMovies = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Speltid = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.lagerSaldoFilmer = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.varuNummerFilmer = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.dataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Plattform = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.lagerSaldoDataspel = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.varuNummerDataspel = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Namn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Pris = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Författare = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Genre = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Format = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Språk = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.lagerSaldoBöcker = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.varuNummerBöcker = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(3, 2);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1320, 630);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Controls.Add(this.dataGridView3);
+            this.tabPage1.Controls.Add(this.label3);
+            this.tabPage1.Controls.Add(this.dataGridView2);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.dataGridView1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(1312, 604);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Lager";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            this.tabPage1.Click += new System.EventHandler(this.tabPage1_Click);
+            // 
+            // dataGridView3
+            // 
+            this.dataGridView3.AllowUserToOrderColumns = true;
+            this.dataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView3.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dataGridViewTextBoxColumn3,
+            this.dataGridViewTextBoxColumn4,
+            this.FormatMovies,
+            this.Speltid,
+            this.lagerSaldoFilmer,
+            this.varuNummerFilmer});
+            this.dataGridView3.Location = new System.Drawing.Point(5, 392);
+            this.dataGridView3.Name = "dataGridView3";
+            this.dataGridView3.Size = new System.Drawing.Size(992, 137);
+            this.dataGridView3.TabIndex = 5;
+            this.dataGridView3.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView3_CellContentClick);
+            // 
+            // dataGridViewTextBoxColumn3
+            // 
+            this.dataGridViewTextBoxColumn3.HeaderText = "Namn";
+            this.dataGridViewTextBoxColumn3.Name = "dataGridViewTextBoxColumn3";
+            this.dataGridViewTextBoxColumn3.Width = 250;
+            // 
+            // dataGridViewTextBoxColumn4
+            // 
+            this.dataGridViewTextBoxColumn4.HeaderText = "Pris";
+            this.dataGridViewTextBoxColumn4.Name = "dataGridViewTextBoxColumn4";
+            // 
+            // FormatMovies
+            // 
+            this.FormatMovies.HeaderText = "Format";
+            this.FormatMovies.Name = "FormatMovies";
+            // 
+            // Speltid
+            // 
+            this.Speltid.HeaderText = "Speltid";
+            this.Speltid.Name = "Speltid";
+            // 
+            // lagerSaldoFilmer
+            // 
+            this.lagerSaldoFilmer.HeaderText = "Antal i lager";
+            this.lagerSaldoFilmer.Name = "lagerSaldoFilmer";
+            // 
+            // varuNummerFilmer
+            // 
+            this.varuNummerFilmer.HeaderText = "Varunummer";
+            this.varuNummerFilmer.Name = "varuNummerFilmer";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(424, 369);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Filmer";
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.AllowUserToOrderColumns = true;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dataGridViewTextBoxColumn1,
+            this.dataGridViewTextBoxColumn2,
+            this.Plattform,
+            this.lagerSaldoDataspel,
+            this.varuNummerDataspel});
+            this.dataGridView2.Location = new System.Drawing.Point(3, 218);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(994, 137);
+            this.dataGridView2.TabIndex = 3;
+            this.dataGridView2.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView2_CellContentClick);
+            // 
+            // dataGridViewTextBoxColumn1
+            // 
+            this.dataGridViewTextBoxColumn1.HeaderText = "Namn";
+            this.dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
+            this.dataGridViewTextBoxColumn1.Width = 250;
+            // 
+            // dataGridViewTextBoxColumn2
+            // 
+            this.dataGridViewTextBoxColumn2.HeaderText = "Pris";
+            this.dataGridViewTextBoxColumn2.Name = "dataGridViewTextBoxColumn2";
+            // 
+            // Plattform
+            // 
+            this.Plattform.HeaderText = "Plattform";
+            this.Plattform.Name = "Plattform";
+            // 
+            // lagerSaldoDataspel
+            // 
+            this.lagerSaldoDataspel.HeaderText = "Antal i lager";
+            this.lagerSaldoDataspel.Name = "lagerSaldoDataspel";
+            // 
+            // varuNummerDataspel
+            // 
+            this.varuNummerDataspel.HeaderText = "Varunummer";
+            this.varuNummerDataspel.Name = "varuNummerDataspel";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(424, 195);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Dataspel";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(424, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Böcker";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToOrderColumns = true;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Namn,
+            this.Pris,
+            this.Författare,
+            this.Genre,
+            this.Format,
+            this.Språk,
+            this.lagerSaldoBöcker,
+            this.varuNummerBöcker});
+            this.dataGridView1.Location = new System.Drawing.Point(5, 44);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(1091, 137);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Namn
+            // 
+            this.Namn.HeaderText = "Namn";
+            this.Namn.Name = "Namn";
+            this.Namn.Width = 250;
+            // 
+            // Pris
+            // 
+            this.Pris.HeaderText = "Pris";
+            this.Pris.Name = "Pris";
+            // 
+            // Författare
+            // 
+            this.Författare.HeaderText = "Författare";
+            this.Författare.Name = "Författare";
+            this.Författare.Width = 200;
+            // 
+            // Genre
+            // 
+            this.Genre.HeaderText = "Genre";
+            this.Genre.Name = "Genre";
+            // 
+            // Format
+            // 
+            this.Format.HeaderText = "Format";
+            this.Format.Name = "Format";
+            // 
+            // Språk
+            // 
+            this.Språk.HeaderText = "Språk";
+            this.Språk.Name = "Språk";
+            // 
+            // lagerSaldoBöcker
+            // 
+            this.lagerSaldoBöcker.HeaderText = "Antal i lager";
+            this.lagerSaldoBöcker.Name = "lagerSaldoBöcker";
+            // 
+            // varuNummerBöcker
+            // 
+            this.varuNummerBöcker.HeaderText = "Varunummer";
+            this.varuNummerBöcker.Name = "varuNummerBöcker";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(1312, 604);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Kassa";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            this.tabPage2.Click += new System.EventHandler(this.tabPage2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.Location = new System.Drawing.Point(548, 550);
+            this.button1.Name = "button1";
+            this.button1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.button1.Size = new System.Drawing.Size(206, 48);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Save";
+            this.button1.TextImageRelation = System.Windows.Forms.TextImageRelation.TextAboveImage;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1335, 644);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        private void tabPage2_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void dataGridView2_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void dataGridView3_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void tabPage1_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView dataGridView3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn FormatMovies;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Speltid;
+        private System.Windows.Forms.DataGridViewTextBoxColumn lagerSaldoFilmer;
+        private System.Windows.Forms.DataGridViewTextBoxColumn varuNummerFilmer;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Plattform;
+        private System.Windows.Forms.DataGridViewTextBoxColumn lagerSaldoDataspel;
+        private System.Windows.Forms.DataGridViewTextBoxColumn varuNummerDataspel;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Namn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Pris;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Författare;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Genre;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Format;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Språk;
+        private System.Windows.Forms.DataGridViewTextBoxColumn lagerSaldoBöcker;
+        private System.Windows.Forms.DataGridViewTextBoxColumn varuNummerBöcker;
+        private Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,299 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CsvHelper;
+using CsvHelper.Configuration;
+using System.Globalization;
+
+namespace Laboration_4
+{
+    public partial class Form1 : Form
+    {
+        //Sstores the data for Books, games and movies.
+        private List<Book> books;
+        private List<Game> games;
+        private List<Movie> movies;
+
+        private CsvConfiguration csvConfig;
+        private CsvWriter csvWriter;
+        private CsvReader csvReaderBook, csvReaderGame, csvReaderMovie;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            csvConfig = new CsvConfiguration(CultureInfo.InvariantCulture)
+            {
+                Delimiter = ",",
+                HasHeaderRecord = true,
+            };
+
+            csvReaderBook = new CsvReader(new StreamReader("bookData.csv"), csvConfig);
+            csvReaderGame = new CsvReader(new StreamReader("gameData.csv"), csvConfig);
+            csvReaderMovie = new CsvReader(new StreamReader("movieData.csv"), csvConfig);
+            csvReaderBook.Context.RegisterClassMap<BookMap>();
+            csvReaderGame.Context.RegisterClassMap<GameMap>();
+            csvReaderMovie.Context.RegisterClassMap<MovieMap>();
+
+            // Initialize the lists here
+            books = new List<Book>();
+            games = new List<Game>();
+            movies = new List<Movie>();
+
+            LoadData();
+        }
+        
+        private void LoadData()
+        {
+            // clear the existing data
+            dataGridView1.Rows.Clear();
+            dataGridView2.Rows.Clear();
+            dataGridView3.Rows.Clear();
+            books.Clear();
+            games.Clear();
+            movies.Clear();
+
+           
+
+            StreamReader reader = null;
+            try
+            {
+                reader = new StreamReader("bookData.csv");
+                using (var csv = new CsvReader(reader, csvConfig))
+                {
+                    csv.Context.RegisterClassMap<BookMap>();
+                    books = csv.GetRecords<Book>().ToList();
+                }
+            }
+            finally
+            {
+                if (reader != null)
+                {
+                    reader.Close();
+                }
+            }
+
+            try
+            {
+                reader = new StreamReader("gameData.csv");
+                using (var csv = new CsvReader(reader, csvConfig))
+                {
+                    csv.Context.RegisterClassMap<GameMap>();
+                    games = csv.GetRecords<Game>().ToList();
+                }
+            }
+            finally
+            {
+                if (reader != null)
+                {
+                    reader.Close();
+                }
+            }
+
+
+            try
+            {
+                reader = new StreamReader("movieData.csv");
+                using (var csv = new CsvReader(reader, csvConfig))
+                {
+                    csv.Context.RegisterClassMap<MovieMap>();
+                    movies = csv.GetRecords<Movie>().ToList();
+                }
+            }
+            finally
+            {
+                if (reader != null)
+                {
+                    reader.Close();
+                }
+            }
+
+            // read the data from the CSV file for books
+            /* using (var reader = new StreamReader("bookData.csv"))
+             using (var csv = new CsvReader(reader, csvConfig))
+             {
+                 csv.Context.RegisterClassMap<BookMap>();
+                 books = csv.GetRecords<Book>().ToList();
+             }
+
+
+
+             // read the data from the CSV file for games
+             using (var reader = new StreamReader("gameData.csv"))
+             using (var csv = new CsvReader(reader, csvConfig))
+             {
+                 csv.Context.RegisterClassMap<GameMap>();
+                 games = csv.GetRecords<Game>().ToList();
+             }
+
+             // read the data from the CSV file for movies
+             using (var reader = new StreamReader("movieData.csv"))
+             using (var csv = new CsvReader(reader, csvConfig))
+             {
+                 csv.Context.RegisterClassMap<MovieMap>();
+                 movies = csv.GetRecords<Movie>().ToList();
+             } */
+
+            // populate the dataGridView1 with the books data
+            foreach (var book in books)
+            {
+                var row = new string[]
+                {
+                    book.Namn,
+                    book.Pris.ToString(),
+                    book.Forfattare,
+                    book.Genre,
+                    book.Format,
+                    book.Sprak
+                };
+                dataGridView1.Rows.Add(row);
+            }
+
+            // populate the dataGridView2 with the games data
+            foreach (var game in games)
+            {
+                var row = new string[]
+                {
+                    game.Namn,
+                    game.Pris.ToString(),
+                    game.Plattform
+                };
+                dataGridView2.Rows.Add(row);
+            }
+
+            // populate the dataGridView3 with the movies data
+            foreach (var movie in movies)
+            {
+                var row = new string[]
+                {
+                    movie.Namn,
+                    movie.Pris.ToString(),
+                    movie.Format,
+                    movie.Speltid
+                };
+                dataGridView3.Rows.Add(row);
+            }  
+        }
+
+        private void SaveData()
+        {
+
+
+            System.Threading.Thread.Sleep(1000);
+
+            using (StreamWriter sw = new StreamWriter("bookData.csv", false, Encoding.UTF8))
+            {
+                using (var csv = new CsvWriter(sw, csvConfig))
+                {
+                    csv.Context.RegisterClassMap<BookMap>();
+                    csv.WriteRecords(books);
+                }
+
+                sw.Close();
+            }
+
+            using (StreamWriter sw = new StreamWriter("gameData.csv", false, Encoding.UTF8))
+            {
+                using (var csv = new CsvWriter(sw, csvConfig))
+                {
+                    csv.Context.RegisterClassMap<GameMap>();
+                    csv.WriteRecords(games);
+                }
+                sw.Close();
+            }
+
+            using (StreamWriter sw = new StreamWriter("movieData.csv", false, Encoding.UTF8))
+            {
+                using (var csv = new CsvWriter(sw, csvConfig))
+                {
+                    csv.Context.RegisterClassMap<MovieMap>();
+                    csv.WriteRecords(movies);
+                }
+                sw.Close();
+            }
+
+            
+        }
+
+
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            SaveData(); // save the data when closing the form
+            base.OnFormClosing(e);
+        }
+
+        // define a class to represent the "Filmer" data
+        public class Movie
+        {
+            public string Namn { get; set; }
+            public int Pris { get; set; }
+            public string Format { get; set; }
+            public string Speltid { get; set; }
+        }
+
+        public class Book
+        {
+            public string Namn { get; set; }
+            public int Pris { get; set; }
+            public string Forfattare { get; set; }
+            public string Genre { get; set; }
+            public string Format { get; set; }
+            public string Sprak { get; set; }
+        }
+
+        public class Game
+        {
+            public string Namn { get; set; }
+            public int Pris { get; set; }
+            public string Plattform { get; set; }
+        }
+
+
+        public class MovieMap : ClassMap<Movie>
+        {
+            public MovieMap()
+            {
+                Map(m => m.Namn).Name("Namn");
+                Map(m => m.Pris).Name("Pris");
+                Map(m => m.Format).Name("Format");
+                Map(m => m.Speltid).Name("Speltid");
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SaveData();
+        }
+
+        public class BookMap : ClassMap<Book>
+        {
+            public BookMap()
+            {
+                Map(m => m.Namn).Name("Namn");
+                Map(m => m.Pris).Name("Pris");
+                Map(m => m.Forfattare).Name("Forfattare");
+                Map(m => m.Genre).Name("Genre");
+                Map(m => m.Format).Name("Format");
+                Map(m => m.Sprak).Name("Sprak");
+            }
+        }
+
+        public class GameMap : ClassMap<Game>
+        {
+            public GameMap()
+            {
+                Map(m => m.Namn).Name("Namn");
+                Map(m => m.Pris).Name("Pris");
+                Map(m => m.Plattform).Name("Plattform");
+            }
+        }
+    }
+}
